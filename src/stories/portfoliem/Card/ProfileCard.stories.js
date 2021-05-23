163fa3f353b4079c7f1aeee14bd69235bf9adad8
Yusuke Miyakawa <chidoriashi00918@gmail.com>
2021-05-23 12:02:23 +0900

    Changing width of the ProfileCard component Fixes #81

--- src/stories/portfoliem/Card/ProfileCard.stories.js
+++ src/stories/portfoliem/Card/ProfileCard.stories.js
@@ -7,7 +7,33 @@ export default {
 
 export const Profile = () => ({
   components: { ProfileCard },
-  template: '<profile-card />'
+  template:
+    '<profile-card :profileImage="profileImage" :actionButtons="socialData" />',
+  data: () => ({
+    profileImage: 'https://avatars.githubusercontent.com/u/3616178?v=4',
+    socialData: [
+      {
+        icon: 'mdi-email',
+        href: 'mailto:chidoriashi00918@gmail.com'
+      },
+      {
+        icon: 'mdi-linkedin',
+        href: 'https://www.linkedin.com/in/yusuke-miyakawa-a0a219191/'
+      },
+      {
+        icon: 'mdi-facebook',
+        href: 'https://www.facebook.com/YuM1909'
+      },
+      {
+        icon: 'mdi-twitter',
+        href: 'https://twitter.com/MiyakawaYusuke'
+      },
+      {
+        icon: 'mdi-github',
+        href: 'https://github.com/chidoriashi1990'
+      }
+    ]
+  })
 });
 
 Profile.storyName = 'Profile Card';
