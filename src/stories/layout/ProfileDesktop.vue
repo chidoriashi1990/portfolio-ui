5812cc03ec3783c9e643ff1ee6f5795c3b1bbbc4
Yusuke Miyakawa <chidoriashi00918@gmail.com>
2021-05-23 12:07:48 +0900

    Changing width of the ProfileDesktop layout component Fixes #82

--- src/stories/layout/ProfileDesktop.vue
+++ src/stories/layout/ProfileDesktop.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container>
     <v-row align="start">
-      <v-col cols="2">
+      <v-col cols="1">
         <div class="d-flex justify-center">
           <v-slot name="left-side"> </v-slot>
         </div>
       </v-col>
-      <v-col cols="8">
+      <v-col cols="10">
         <div class="d-flex justify-center">
           <div class="d-flex flex-column">
             <slot name="main">
@@ -17,7 +17,7 @@
           </div>
         </div>
       </v-col>
-      <v-col cols="2">
+      <v-col cols="1">
         <div class="d-flex justify-center">
           <v-slot name="right-side">
             <v-skeleton-loader type="card" v-bind="switchAttrs" />
@@ -34,7 +34,7 @@ export default {
   data: () => ({
     cardAttrs: {
       class: 'mb-12',
-      width: '450',
+      width: '500',
       boilerplate: true,
       elevation: 2
     },
