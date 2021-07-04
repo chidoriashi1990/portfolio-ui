5e1ad25ba410f9cd55b96029cb1ef3ebe146ca93
Yusuke Miyakawa <chidoriashi00918@gmail.com>
2021-07-04 11:32:57 +0900

    Add a profile card component for mobile. Fixes #108

---
+++ src/stories/portfoliem/Card/ProfileMobileCard.stories.js
@@ -0,0 +1,44 @@
+import ProfileMobileCard from './ProfileMobileCard';
+
+export default {
+  title: 'Portfoliem/Card',
+  component: ProfileMobileCard
+};
+
+export const Profile = () => ({
+  components: { ProfileMobileCard },
+  template:
+    '<profile-mobile-card :profileImage="profileImage" :actionButtons="socialData" />',
+  data: () => ({
+    profileImage: 'https://avatars.githubusercontent.com/u/3616178?v=4',
+    socialData: [
+      {
+        icon: 'mdi-email',
+        text: 'Mail',
+        href: 'mailto:chidoriashi00918@gmail.com'
+      },
+      {
+        icon: 'mdi-linkedin',
+        text: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/yusuke-miyakawa-a0a219191/'
+      },
+      {
+        icon: 'mdi-facebook',
+        text: 'Facebook',
+        href: 'https://www.facebook.com/YuM1909'
+      },
+      {
+        icon: 'mdi-twitter',
+        text: 'Twitter',
+        href: 'https://twitter.com/MiyakawaYusuke'
+      },
+      {
+        icon: 'mdi-github',
+        text: 'GitHub',
+        href: 'https://github.com/chidoriashi1990'
+      }
+    ]
+  })
+});
+
+Profile.storyName = 'Profile Mobile Card';
---
+++ src/stories/portfoliem/Card/ProfileMobileCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card class="d-flex justify-space-between mb-6" flat>
+    <v-card width="290px" class="pa-5">
+      <center>
+        <v-card elevation="0" class="px-1">
+          <v-img
+            :src="profileImage"
+            alt="Profile image"
+            :aspect-ratio="1 / 1"
+            width="200"
+          ></v-img>
+        </v-card>
+      </center>
+
+      <p class="headline font-weight-bold text-center py-3 ma-0">
+        Yusuke Miyakawa
+      </p>
+
+      <p class="text-center pb-3 ma-0">
+        Software Engineer & Web Development.
+      </p>
+
+      <div
+        v-for="button in actionButtons"
+        :key="button.icon"
+        class="text-center my-3"
+      >
+        <v-btn
+          large
+          dark
+          class="text-capitalize font-weight-bold"
+          :href="button.href"
+          target="_blank"
+          block
+        >
+          <v-icon dark left>{{ button.icon }}</v-icon>
+          {{ button.text }}
+        </v-btn>
+      </div>
+    </v-card>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ProfileCard',
+  props: {
+    profileImage: {
+      type: String,
+      required: true
+    },
+    actionButtons: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
